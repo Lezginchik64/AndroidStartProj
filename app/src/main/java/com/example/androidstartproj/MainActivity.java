2bf8402db90a92502c3ed3478a5d3cc788931ee6
Vadim <vadim.pirimov.01@mail.ru>
2023-03-17 15:42:56 +0400

    Added Java screen configuration example with Constraint Layout

--- app/src/main/java/com/example/androidstartproj/MainActivity.java
+++ app/src/main/java/com/example/androidstartproj/MainActivity.java
@@ -11,20 +11,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        ConstraintLayout constraintLayout = new ConstraintLayout(this);
-        TextView textView = new TextView(this);
-        textView.setText("Hello Android!");
-        textView.setTextSize(26);
+        setContentView(R.layout.second_layout);
+        TextView textView = (TextView) findViewById(R.id.header);
+        textView.setText("Hello from Java");
 
-        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
-        //wrap_content – ширина/высота элемента будет определяться его содержимым
-        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
-        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-        textView.setLayoutParams(layoutParams);
-        constraintLayout.addView(textView);
 
-        setContentView(constraintLayout);
+//        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+//        TextView textView = new TextView(this);
+//        textView.setText("Hello Android!");
+//        textView.setTextSize(26);
+
+//        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+//        //wrap_content – ширина/высота элемента будет определяться его содержимым
+//        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+//        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+//        textView.setLayoutParams(layoutParams);
+//        constraintLayout.addView(textView);
+//
+//        setContentView(constraintLayout);
 
     }
 }
