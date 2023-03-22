8bb0c423fd572a530780857fddfa60f6d7635a88
Vadim <vadim.pirimov.01@mail.ru>
2023-03-23 01:09:48 +0400

    First screen

--- app/src/main/java/com/example/androidstartproj/MainActivity.java
+++ app/src/main/java/com/example/androidstartproj/MainActivity.java
@@ -11,24 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//        TextView textView = (TextView) findViewById(R.id.header);
-//        textView.setText("Hello from Java");
-
-
-//        ConstraintLayout constraintLayout = new ConstraintLayout(this);
-//        TextView textView = new TextView(this);
-//        textView.setText("Hello Android!");
-//        textView.setTextSize(26);
-
-//        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
-//        //wrap_content – ширина/высота элемента будет определяться его содержимым
-//        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
-//        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-//        textView.setLayoutParams(layoutParams);
-//        constraintLayout.addView(textView);
-//
-//        setContentView(constraintLayout);
+        setContentView(R.layout.second_layout);
 
     }
 }
